e7ec62ea53b5889157dbf96afe735d53c3f0225a
rgnaik <rgnaik@umich.edu>
2020-05-05 18:35:21 -0400

    Can add text above image

--- script.js
+++ script.js
@@ -5,14 +5,19 @@ const App = new Vue({
       query: "",
       response: null, 
       photos: null,
+      selected_photo: null,
+      is_selected: false,
+      meme_text: "",
     },
     methods: {
         search_call() {
             /*Key: e0019bee21d189d5402906185b8837a1
               Secret: a877828bd6cd0862 */
 
-            axios
-                .get("https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=e0019bee21d189d5402906185b8837a1&format=json&per_page=5&text=" + this.query)
+            this.is_selected = false;
+            
+              axios
+                .get("https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=e0019bee21d189d5402906185b8837a1&format=json&per_page=50&text=" + this.query)
                 .then(response => {
                     let str = response.data;
 
@@ -20,12 +25,30 @@ const App = new Vue({
                     str = str.substr(0, str.length - 1);
                     this.response = JSON.parse(str.substr(14));
                     this.photos = this.response.photos.photo;
-                    console.log(this.photos);
                 })
         },
 
         get_img_url(photo) {
-            return "https://farm" + photo.farm + ".staticflickr.com/" + photo.server + "/" + photo.id + "_" + photo.secret + "_n.jpg";
+            //returns url for photo according to flickr api: https://www.flickr.com/services/api/misc.urls.html
+            return "https://farm" + photo.farm + ".staticflickr.com/" + photo.server + "/" + photo.id + "_" + photo.secret + "_q.jpg";
+        },
+
+        get_full_img_url(photo) {
+             //returns url for photo according to flickr api: https://www.flickr.com/services/api/misc.urls.html
+             return "https://farm" + photo.farm + ".staticflickr.com/" + photo.server + "/" + photo.id + "_" + photo.secret + "_n.jpg";
+        },
+
+        photo_click(photo) {
+            this.selected_photo = photo;
+            this.is_selected = true;
+        },
+
+        home_click() {
+            this.is_selected = false;
+
+            //reset query
+            this.query = "";
+            this.photos = null;
         }
     }
   })
